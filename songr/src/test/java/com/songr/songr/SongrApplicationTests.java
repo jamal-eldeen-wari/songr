1cc3b4ad184746fc635782625e2eac0c3e973a61
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-10-15 20:06:28 +0300

    adding the integeration testing to the code

--- songr/src/test/java/com/songr/songr/SongrApplicationTests.java
+++ songr/src/test/java/com/songr/songr/SongrApplicationTests.java
@@ -1,9 +1,12 @@
 package com.songr.songr;
 
-import com.songr.songr.model.Album;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 ;
 import static org.hamcrest.Matchers.containsString;
@@ -12,9 +15,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 class SongrApplicationTests {
+
 	@Autowired
 	private MockMvc mockMvc;
 	@Test
